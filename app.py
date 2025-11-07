6ab89644f8ee3252ccd0148f4da3fb6fe63f07a5
Siddhant128-bit <siddhant.sharma1974@gmail.com>
2025-11-07 22:56:36 +0545

    Encrypt Decrypt pages added

--- app.py
+++ app.py
@@ -1,3 +1,7 @@
 import streamlit as st
 
 st.title("Encrypt iT")
+#Basic side bar consisting of all the pages
+with st.sidebar:
+    st.title('Encrypt iT')
+    page=st.radio('Go To', ["Encrypt","Decrypt"])
