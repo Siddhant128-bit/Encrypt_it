bb3d231dce086eb282a3332e1e371ef3c7b8ecb3
Siddhant128-bit <siddhant.sharma1974@gmail.com>
2025-11-07 22:57:59 +0545

    Tested Encryption page and decryption page basic OK

--- app.py
+++ app.py
@@ -1,7 +1,16 @@
 import streamlit as st
 
 st.title("Encrypt iT")
+
 #Basic side bar consisting of all the pages
 with st.sidebar:
     st.title('Encrypt iT')
     page=st.radio('Go To', ["Encrypt","Decrypt"])
+
+
+if page=='Encrypt':
+    st.title('Encryption')
+
+
+if page=='Decrypt':
+    st.title('Decryption')
